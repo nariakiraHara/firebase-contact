05e1b7c7c37977eb4d35b05b0c0ce7eff7eb5257
github nariakiraHara <nariakra@gmail.com>
2018-01-07 21:52:20 +0900

    first commit

---
+++ JS/inquiry.js
@@ -0,0 +1,44 @@
+// 取得したクライアントID
+var clientid = '708484824182-jo8temsd8ddea99eppdih3hfvi01gf5b.apps.googleusercontent.com'; 
+// スコープを配列で指定して分解
+var scopes = ['https://www.googleapis.com/auth/gmail.send'].join(' '); 
+
+function onLoadCallbackFunction() {
+    gapi.auth.authorize({'client_id': clientid, 'scope': scopes, 'immediate': true}, handleAuthResult);
+}
+
+function handleAuthResult(authResult) {
+    if (authResult && !authResult.error) {
+      // 認証後の処理
+    } else {
+      // 認証できていない時やエラーの時の処理
+    }
+}
+
+function inquiryProcessing(strContent) {
+    var bSend = false;
+    bsend = gapi.client.load('gmail', 'v1', callbackFunction(strContent));
+    return bsend;
+}
+
+    
+function callbackFunction(strContent) {
+    var mimeData = ["To: nariakra@gmail.com",
+        "Subject: =?utf-8?B?" + window.btoa(unescape(encodeURIComponent("お取合せフォームからのメッセージ"))) + "?=",
+        "MIME-Version: 1.0",
+        "Content-Type: text/plain; charset=UTF-8",
+        "Content-Transfer-Encoding: 7bit",
+            "",
+        strContent].join("\n").trim();
+
+        var raw = window.btoa(unescape(encodeURIComponent(mimeData))).replace(/\+/g, '-').replace(/\//g, '_');
+        gapi.client.gmail.users.messages.send({
+            'userId': 'me',
+                'resource': {
+                'raw': raw
+            }
+        }).execute(function() {
+            // 送信後の処理
+            return true;
+        });
+}
---
+++ JS/main.js
@@ -0,0 +1,46 @@
+// DOMの操作
+var strDisName = document.getElementById('name');
+var strEmail = document.getElementById('eMail');
+
+// 初期化
+var strAddress = "";
+var strUserName = "";
+var strUserId = "";
+function init(user) {
+    strUserId = user.uid
+    strAddress = user.email;
+    strUserName = user.displayName;
+    strEmail.innerHTML = user.email;
+    strDisName.innerHTML = user.displayName;
+}
+
+// 送信ボタン押下時の処理
+function sendMessage() {
+    // textareaのメッセージを取得
+    var strContent = document.getElementById('content').value;
+
+    console.log(strContent);
+    if (strContent == "" || strContent == null) {
+        alert("メッセージを入力してください。");
+        return;
+    }
+
+    if (confirm("メッセージを送信してよろしいですか？")) {
+        // メールに記載したい情報
+        var IsSendComp = inquiryProcessing(strContent);
+        writeUserData(IsSendComp);
+    } else {
+        return
+    }
+
+    function writeUserData(bSend) {
+        firebase.database().ref('contact/').set({
+            username: strUserName,
+            email: strEmail,
+            message: strContent,
+            userid: strUserId,
+            sendFlag: bSend
+        });
+    }
+}
+
